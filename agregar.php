7ac310d9e90e289ab845f61512b5c22eeacac427
Haziel <hazielmaldonado21@aragon.unam.mx>
2023-11-29 19:13:34 -0600

    Cambio 16

--- agregar.php
+++ agregar.php
@@ -16,3 +16,25 @@
             $correo=$_POST['correo'];
             $nombre_usuario=$_POST['nombre_usuario'];
             $password=$_POST['password'];
+            
+            include("conexion.php");
+
+            $sql= "insert into usuarios(nombre,direccion,telefono,correo,nombre_usuario,password) values('".$nombre."','".$direccion."','".$telefono."','".$correo."','".$nombre_usuario."','".$password."')";
+
+            $resultado=mysqli_query($conexion,$sql);
+            
+            if($resultado){
+                echo " <script language = 'JavaScript'>
+                        alert('Los datos fueron ingresados correctamente a la BD');
+                        location.assign('index.php');
+                        </script>";
+
+            }else {
+
+                echo " <script language = 'JavaScript'>
+                        alert('Los datos NO fueron ingresados correctamente a la BD');
+                        location.assign('index.php');
+                        </script>";
+
+            }
+
